1e3b4a56db1211cafe6d69140b3e7ce8589826d0
Alvy Rahman <mdalvyrahman@gmail.com>
2021-04-24 16:56:56 +0600

    Create home.php

---
+++ LabTask4/home.php
@@ -0,0 +1,27 @@
+<?php
+?>
+
+<html>
+<head>
+    <title>Home</title>
+</head>
+
+<body>
+    <header style="padding:10px">
+        <img src="logo.PNG" alt="logo" style="width:90px;height:50px;">
+        <article style="text-align: right;">
+        <a href="home.php">Home</a>
+        <a href="login.php">Login</a>
+        <a href="registration.php">Registration</a>
+        </article>
+    </header>
+
+    <section style="padding:150px;">
+        <h2 style="text-align: center;">Welcome to AtoZ.com</h2>
+    </section>
+
+    <footer style="text-align: center; padding:20px;">
+        <p>Copyright 2021</p>
+    </footer>
+</body>
+</html>
